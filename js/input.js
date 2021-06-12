ed86d1cb3b9fdc55d59d3cfedcdb5ef363660262
DePianoman <depianoman2004@gmail.com>
2021-06-11 21:14:24 -0400

    Update style

--- js/input.js
+++ js/input.js
@@ -2,7 +2,10 @@ const sqlPromise = initSqlJs({
     locateFile: file => `./${file}`
 });
 const dataPromise = fetch("./data/addresses.db").then(res => res.arrayBuffer());
-var db = undefined;
+var db;
+var reader = new commonmark.Parser();
+var writer = new commonmark.HtmlRenderer();
+var cookie;
 
 async function init() {
     const [SQL, buf] = await Promise.all([sqlPromise, dataPromise]);
@@ -10,18 +13,29 @@ async function init() {
 }
 
 $(document).ready(function() {
-    init();
+    cookie = getCookie("theme");
+    document.getElementById('switch').innerHTML = `${cookie == "Light" ? "Dark" : "Light"} Theme`;
+
+    document.querySelectorAll(`link[title="${cookie}"]`).forEach(element => element.removeAttribute("disabled"));
+    document.querySelectorAll(`link[title="${cookie == "Light" ? "Dark" : "Light"}"]`).forEach(element => element.setAttribute("disabled", "disabled"));
+
+    init().then(() => {
+        generate("");
+    })
+
     $('#userin').on('input', function() {
         var dInput = this.value;
-        getPHP(dInput)
-            .then((phpOutput) => {
-                $('#output-p').text("");
-                if (dInput != "") {
-                    createResults(phpOutput);
-                }
-            });
+        generate(dInput);
     });
 });
+
+function generate(input) {
+    getPHP(input)
+            .then((phpOutput) => {
+                $('#left').html("");
+                createResults(phpOutput);
+            });
+}
 
 async function getPHP(dInput) {
     var stmt = db.prepare("SELECT * FROM ADDRESSES WHERE NAME LIKE $name OR ADDRESS LIKE $name");
@@ -43,6 +57,20 @@ async function getPHP(dInput) {
 
 }
 
+function switchTheme() {
+    if (cookie == "Dark") {
+        createCookie("theme", "Light", 1000);
+    } else {
+        createCookie("theme", "Dark", 1000);
+    }
+
+    document.querySelectorAll(`link[title="${cookie}"]`).forEach(element => element.setAttribute("disabled", "disabled"));
+    cookie = getCookie("theme");
+    document.querySelectorAll(`link[title="${cookie}"]`).forEach(element => element.removeAttribute("disabled"));
+
+    document.getElementById('switch').innerHTML = `${cookie == "Light" ? "Dark" : "Light"} Theme`;
+}
+
 function createResults(arr) {
     arr.forEach(element => {
         displayResult(element);
@@ -50,8 +78,11 @@ function createResults(arr) {
 }
 
 function displayResult(element) {
-    var currText = $('#output-p').text();
-    $('#output-p').append(`${element.name} | ${element.address} | ${element.params} | ${getType(element.type)}<br />`);
+    $('#left').append(formatEntry(element));
+}
+
+function formatEntry(element) {
+    return `<button id="${element.address}" class="left-button">${element.name} | ${element.address} | ${element.params} | ${getType(element.type)}</button><br />`
 }
 
 function getType(type) {
@@ -63,4 +94,55 @@ function getType(type) {
         case 2:
             return "__fastcall";
     }
+}
+
+function showRight(markdown) {
+    var parsed = reader.parse(markdown);
+    $('#right').html(writer.render(parsed));
+    hljs.highlightAll();
+}
+
+$(document).on('click', function(e) {
+    var btn = $(e.target);
+    var className = btn.attr("class");
+    if (className == "left-button") {
+        $('#right').show();
+        var id = btn.attr("id");
+        var stmt = db.prepare("SELECT * FROM ADDRESSES WHERE ADDRESS IS $id");
+        stmt.bind({$id:id});
+        if (stmt.step()) {
+            var row = stmt.getAsObject();
+            showRight(row.MD);
+        }
+    } else if (!$("#right").get(0).contains(e.target)) {
+        $('#right').hide();
+    }
+})
+
+var createCookie = function(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
 }
